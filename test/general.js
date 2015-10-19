6868c7d6ee5d561cf85cd123e186f75d08dcf19b
Lyza Danger Gardner <lyza@lyza.com>
2015-10-19 14:00:31 -0700

    Add mocha, first test

---
+++ test/general.js
@@ -0,0 +1,27 @@
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-assert');
+
+describe('general', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(__dirname, 'temp'))
+      .on('end', done);
+  });
+
+  it('the generator can be required without throwing', function () {
+    // not testing the actual run of generators yet
+    require('../app');
+  });
+
+  it('creates expected files', function () {
+    assert.file([
+      'package.json',
+      'gulp.config.js',
+      'gulpfile.babel.js',
+      '.gitignore',
+      '.node-version'
+    ]);
+  });
+});
