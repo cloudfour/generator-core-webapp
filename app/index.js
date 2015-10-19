e299e7953e2e78fc27b2f6922ce06dc005c1db7f
Lyza Danger Gardner <lyza@lyza.com>
2015-10-19 11:08:10 -0700

    Add templated _package.json and write it

--- app/index.js
+++ app/index.js
@@ -6,6 +6,14 @@ module.exports = generators.Base.extend({
   constructor: function () {
     generators.Base.apply(this, arguments);
   },
+  writing: {
+    packageJSON: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+    }
+  },
   end: {
     sayBye: function () {
       this.log('All done! Bye, now!');
