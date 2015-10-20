ad32593c0bfa70669320fa0713d25ea9b678bfd2
Lyza Danger Gardner <lyza@lyza.com>
2015-10-20 11:15:20 -0700

    Add pending test

--- test/general.js
+++ test/general.js
@@ -26,4 +26,13 @@ describe('general', function () {
       'src/main.css'
     ]);
   });
+
+  it('needs to have package.json/npmInstall tested');
+  /**
+   * I believe I can do this by mocking `npmInstall`
+   * the way https://github.com/yeoman/generator-bootstrap/blob/master/test/test.js
+   * mocks `bowerInstall`. And ideally use `sinon` to
+   * spy on how it's invoked.
+   **/
+
 });
